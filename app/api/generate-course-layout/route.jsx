7dd575082f0996a9c9fc0caa98b5a867c9cc1dae
Aryan0512398 <aryangupta052015@gmail.com>
2026-04-07 01:42:52 +0530

    Update  packages

--- app/api/generate-course-layout/route.jsx
+++ app/api/generate-course-layout/route.jsx
@@ -47,7 +47,7 @@ export async function POST(req) {
       apiKey: process.env.GEMINI_API_KEY,
     });
 
-    const model = "gemini-2.0-flash";
+    const model = "gemini-2.5-flash";
 
     const contents = [
       {
